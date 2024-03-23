d62bda18302898d08a69d96f2ec5955d28d2ef01
김태우 <kimtaewoo721@gmail.com>
2024-03-23 15:13:44 +0900

    별 찍기-1

--- Loop/Main.cpp
+++ Loop/Main.cpp
@@ -1,18 +1,34 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int N;
+//	cin >> N;
+//
+//	for (int i = 1; i <= N; i++)
+//	{
+//		for (int j = 0; j < i; j++) 
+//		{
+//			cout << "*";
+//		}
+//		cout << endl;
+//	}
+//
+//	return 0;
+//}
+
 #include <iostream>
 using namespace std;
 
 int main() {
+	string star = "";
 	int N;
 	cin >> N;
 
-	for (int i = 1; i <= N; i++)
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < i; j++) 
-		{
-			cout << "*";
-		}
-		cout << endl;
+		star += "*";
+		cout << star << endl;
 	}
-
 	return 0;
 }
